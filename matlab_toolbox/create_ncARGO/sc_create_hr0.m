d5d9b699a833f9e7670594140cfa9d69b9f7fb02
batpic <39376092+batpic@users.noreply.github.com>
2018-07-16 14:30:36 +0200

    Add files via upload

--- matlab_toolbox/create_ncARGO/sc_create_hr0.m
+++ matlab_toolbox/create_ncARGO/sc_create_hr0.m
@@ -49,6 +49,13 @@ for index=1:length(list_tag),
         Omask=double(Mqc.DOXY_QC==1);
         DOXY(Omask==0) = NaN;
     end    
+    LIGHT=Mqc.PRES*NaN;
+    if isfield(Mqc,'LIGHT'),
+        LIGHT=Mqc.LIGHT;
+        Mqc.LIGHT_QC(Mqc.LIGHT_QC==0) = 1;
+        Lmask=double(Mqc.LIGHT_QC==1);
+        LIGHT(Lmask==0) = NaN;
+    end
     
     std_lev = [1:1000]';
     sc_profiles_interp;
@@ -58,38 +65,39 @@ for index=1:length(list_tag),
     S=SALI_std_lev;
     F=CHLA_std_lev;
     O=DOXY_std_lev;
-    
+    L=LIGHT_std_lev;
     %% write fcell files
     N=size(hi,1);
     PTi=cell(1,N); PSi=cell(1,N);
-    PFi=cell(1,N); POi=cell(1,N);    
+    PFi=cell(1,N); POi=cell(1,N); 
+    PLi=cell(1,N);
     for ii=1:N,        
         PTi{ii}=[P(:,ii) T(:,ii)];
         PSi{ii}=[P(:,ii) S(:,ii)];
         PFi{ii}=[P(:,ii) F(:,ii)];
         POi{ii}=[P(:,ii) O(:,ii)];
+        PLi{ii}=[P(:,ii) L(:,ii)];
         hi(ii,8)=sum(~isnan(T(:,ii)));        
     end
     
     %save fcell
     name_fcell=[conf.temporary_fcell smru_name '_hr0_fcell.mat'];
-    save(name_fcell,'hi','hs','PTi','PSi','PFi','POi','EXP','PI','NATION','isoxy','isfluo');
+    save(name_fcell,'hi','hs','PTi','PSi','PFi','POi','PLi','EXP','PI','NATION','isoxy','isfluo','islight');
     
     %% save in Argo netcdf format
     if length(hi)>0
         suffix = 'hr0_prof.nc';
         convert_fcell2ARGO(conf,info_deployment.EXP,name_fcell,suffix,1000);
+        %% set masks
+        Mqc.PRES_QC=double(~isnan(PRES_std_lev)); Mqc.PRES_QC(Mqc.PRES_QC==0)=9;
+        Mqc.TEMP_QC=double(~isnan(TEMP_std_lev)); Mqc.TEMP_QC(Mqc.TEMP_QC==0)=9;
+        Mqc.PSAL_QC=double(~isnan(SALI_std_lev)); Mqc.PSAL_QC(Mqc.PSAL_QC==0)=9;
+        Mqc.CHLA_QC=double(~isnan(CHLA_std_lev)); Mqc.CHLA_QC(Mqc.CHLA_QC==0)=9;
+        Mqc.DOXY_QC=double(~isnan(DOXY_std_lev)); Mqc.DOXY_QC(Mqc.DOXY_QC==0)=9;
+        Mqc.LIGHT_QC=double(~isnan(LIGHT_std_lev)); Mqc.LIGHT_QC(Mqc.LIGHT_QC==0)=9;
+        
+        name_prof_hr0 = sprintf('%s%s%s_prof.nc',info_deployment.dir,smru_name,'_hr0');
+        ARGO_save_qc(name_prof_hr0,Mqc,0);
     end
-
-    %% set masks
-    
-    Mqc.PRES_QC=double(~isnan(PRES_std_lev)); Mqc.PRES_QC(Mqc.PRES_QC==0)=9;
-    Mqc.TEMP_QC=double(~isnan(TEMP_std_lev)); Mqc.TEMP_QC(Mqc.TEMP_QC==0)=9;
-    Mqc.PSAL_QC=double(~isnan(SALI_std_lev)); Mqc.PSAL_QC(Mqc.PSAL_QC==0)=9;
-    Mqc.CHLA_QC=double(~isnan(CHLA_std_lev)); Mqc.CHLA_QC(Mqc.CHLA_QC==0)=9;
-    Mqc.DOXY_QC=double(~isnan(DOXY_std_lev)); Mqc.DOXY_QC(Mqc.DOXY_QC==0)=9;
-
-    name_prof_hr0 = sprintf('%s%s%s_prof.nc',info_deployment.dir,smru_name,'_hr0');
-    ARGO_save_qc(name_prof_hr0,Mqc,0);
 
 end
